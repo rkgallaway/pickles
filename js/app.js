3cc3d9da513037baa24c277b8740203ef17ccc52
Ryan Gallaway <rkgallaway@msn.com>
2019-09-27 10:15:55 -0700

    completed demo

---
+++ js/app.js
@@ -0,0 +1,45 @@
+function addName(){
+  var userName = prompt('Hello, what is your name?');
+  return 'Hello ' + userName + '!  Here is your order!'
+}
+
+function howMany(){
+  var count = prompt('How many do you want to order?');
+  while(count === '' || isNaN(count)){
+    count = prompt('Please enter a number.  How many do you want to order?');
+  }
+  return Number(count);
+}
+
+function getItem(){
+  var order = '';
+  var item;
+  
+  while (order !== 'pickles' && order !== 'beets') {
+    order = prompt('Please enter "pickles" or "beets"...');
+  }
+
+  if (order === 'pickles'){
+    item = '<img style="border:none;" src="images/pickle.png">';
+  } else if (order === 'beets'){
+    item = '<img style="border:none;" src="images/beet.png">';
+  } 
+  return item;
+}
+
+function showOrder(){
+  var item = getItem();
+  var total = howMany();
+  var result = '';
+
+  for(var i = 0; i < total; i++){
+    var realCount = i + 1;
+    result = result + '<p style="width: 150px; display: inline-block; border: 1px solid black;">Model #' + realCount + ' ' + item + '</p>'
+  }
+  return result;
+}
+
+
+
+
+
